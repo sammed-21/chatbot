6972a5787de2861db2e025bf231b0e0bf1b758c1
sammed-21 <sammed2110@gmail.com>
2023-08-26 15:24:09 +0530

    compelete User entity

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,7 +1,9 @@
-const { useCallback } = require("react");
 const User = require("../models/User");
 
 const UserController = {
+  // @Desc get all users
+  // @route GET /users
+  // @public
   getUsers: async (req, res) => {
     try {
       console.log("inside getalluser");
@@ -12,12 +14,82 @@ const UserController = {
     }
   },
 
+  // @Desc create new users
+  // @route Post /users
+  // @public
+
   createUser: async (req, res) => {
     try {
       console.log("users router gate or User model");
       await User.create(req.body);
       console.log("user is created");
       return res.json("user is created");
+    } catch (error) {
+      res.status(500).json({ message: "Error fetching users" });
+    }
+  },
+
+  // @Desc get users by id
+  // @route get /users/:id
+  // @public
+
+  getUserById: async (req, res) => {
+    try {
+      const userId = req.params.id;
+      const user = await User.findByPk(userId);
+
+      if (!user) {
+        return res.status(404).json({ message: "User not found" });
+      }
+      return res.status(200).json(user);
+    } catch (error) {
+      res.status(500).json({ message: "Error fetching users" });
+    }
+  },
+
+  // @Desc PUT users by id
+  // @route PUT /users/:id
+  // @public
+
+  updateUserById: async (req, res) => {
+    try {
+      const userId = req.params.id;
+      const reqs = req.body;
+
+      //   const user = await User.findByPk(userId);
+      const user = await User.findByPk(userId);
+      if (!user) {
+        res.status(400).json({ message: "User not found" });
+      }
+      user.username = reqs.username;
+      user.email = reqs.email;
+      user.save();
+
+      //   if (!user) {
+      //     return res.status(404).json({ message: "User not found" });
+      //   }
+      return res.status(200).json(user);
+    } catch (error) {
+      res.status(500).json({ message: "Error fetching users" });
+    }
+  },
+  findUserByIdAndDelete: async (req, res) => {
+    try {
+      const userId = req.params.id;
+  
+
+        const user = await User.findByPk(userId);
+      
+      if (!user) {
+        res.status(400).json({ message: "User not found" });
+      }
+      
+        user.destroy();
+
+      //   if (!user) {
+      //     return res.status(404).json({ message: "User not found" });
+      //   }
+      return res.status(200).json({message:"user is been deleted"});
     } catch (error) {
       res.status(500).json({ message: "Error fetching users" });
     }
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,10 +1,11 @@
-const express = require('express')
-// const User = require('../models/User');
-const router = express.Router()
-const UserController = require("../controllers/UserController")
-// const router = express.Router()
- 
+const express = require("express");
+
+const router = express.Router();
+const UserController = require("../controllers/UserController");
+
 router.get("/", UserController.getUsers);
 router.post("/", UserController.createUser);
-  
+router.get("/:id", UserController.getUserById);
+router.put("/:id", UserController.updateUserById);
+router.delete("/:id", UserController.findUserByIdAndDelete);
 module.exports = router;
